0dc9985a5fc6f6868b642d39cc64e6ed1aff3e12
ACADEMY-7464B36\Administrator <annapiatoresson@hotmail.com>
2018-04-16 11:55:44 +0200

    First commit

--- EfSamurai1/Class1.cs
+++ EfSamurai1/Class1.cs
@@ -1,8 +1,24 @@
-﻿using System;
+﻿using EfSamurai.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace EfSamurai1
 {
     public class Class1
     {
+    }
+
+    public class SamuraiContext : DbContext
+    {
+        
+            public DbSet<Samurai> Samurais { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer(
+                  "Server = (localdb)\\mssqllocaldb; Database = EfSamurai; Trusted_Connection = True; ");
+            }
+        
+
     }
 }
